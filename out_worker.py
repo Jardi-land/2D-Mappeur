f016992572c20e40caa6415150be3fbed7061405
Jardiland <lorenzodezen@outlook.com>
2022-03-23 10:39:07 +0100

    changement de tools avec des raccourcis clavier

--- out_worker.py
+++ out_worker.py
@@ -62,17 +62,18 @@ class out_worker:
         #All button
         self.hand_tool = button(self.display_surface, "hand_tool", "mappeur_files/internal/out_worker/button/hand_tool/unselected.png", "mappeur_files/internal/out_worker/button/hand_tool/selected.png", self.first_button_pos[0], self.first_button_pos[1], self.button_size[0], self.button_size[1])
         self.zoom_tool = button(self.display_surface, "zoom_tool", "mappeur_files/internal/out_worker/button/zoom_tool/unselected.png", "mappeur_files/internal/out_worker/button/zoom_tool/selected.png", self.first_button_pos[0] - (self.button_size[0] - 2), self.first_button_pos[1], self.button_size[0], self.button_size[1])
-
+    
     def send_tool(self):
         return self.current_tool
 
     def update(self):
         self.mouse = pygame.mouse.get_pos()
+        self.keys = pygame.key.get_pressed()
 
-        if not self.hand_tool.get_tool(self.mouse, self.current_tool) == None:
-            self.current_tool = self.hand_tool.get_tool(self.mouse, self.current_tool)
+        if not self.hand_tool.get_tool(self.mouse, self.current_tool) == None: self.current_tool = self.hand_tool.get_tool(self.mouse, self.current_tool)
+        if self.keys[pygame.K_h]: self.current_tool = "hand_tool"
         self.hand_tool.update(self.mouse, self.current_tool)
 
-        if not self.zoom_tool.get_tool(self.mouse, self.current_tool) == None:
-            self.current_tool = self.zoom_tool.get_tool(self.mouse, self.current_tool)
+        if not self.zoom_tool.get_tool(self.mouse, self.current_tool) == None: self.current_tool = self.zoom_tool.get_tool(self.mouse, self.current_tool)
+        if self.keys[pygame.K_z]: self.current_tool = "zoom_tool"
         self.zoom_tool.update(self.mouse, self.current_tool)
