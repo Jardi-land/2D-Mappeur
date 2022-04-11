8ea65adbe0427692c26eb7b3fbad19b593dbca08
Jardiland <lorenzodezen@outlook.com>
2022-04-11 12:08:36 +0200

    Updated settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 #Variable de base pour la definition de l'ecran
 screen_res_array = {1:[1920, 1080],2:[1536, 864],3:[1152, 648],4:[768, 432],5:[384, 216]}
-screen_res_numb = 2 # 2 or lower !! not >2 !!
+screen_res_numb = 3 # 2 or lower !! not >2 !!
 screen_res = screen_res_array[screen_res_numb]
 screen_scale = screen_res[0]/1920
 
