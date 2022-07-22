27b8afceb7cff73b63303ded06ced816f7bc118e
Jardiland <lorenzodezen@outlook.com>
2022-07-22 14:41:20 +0200

    Update de la surface pour la e_list

--- e_list.py
+++ e_list.py
@@ -10,5 +10,14 @@ class e_list:
         self.background = pygame.Surface((screen_res[0] - self.wk_zone_res[0] - 30, screen_res[1] - 6))
         self.background.fill(((198, 198, 198, 255)))
 
+        self.active = False
+
+        self.e_list_surface = pygame.Surface((screen_res[0] - self.wk_zone_res[0] - 30, screen_res[1] - 6))
+
+    def display_OnScreen(self):
+        self.display_surface.blit(self.e_list_surface, (3, 3))
+    
     def draw(self):
-        self.display_surface.blit(self.background, (3, 3))
+        self.e_list_surface.blit(self.background, (0, 0))
+
+        self.display_OnScreen()
